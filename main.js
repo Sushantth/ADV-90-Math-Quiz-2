08ec6147d28db682cede471480682a1151ca800e
Sushantth <98395347+Sushantth@users.noreply.github.com>
2024-03-21 16:26:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function add_user () {
+location.replace("http://127.0.0.1:5500/quiz_game_page.html");
+var firstname = document.getElementById(Player_1);
+localStorage.setItem(Player_1);
+var secondname = document.getElementById(Player_2);
+localStorage.setItem(Player_2);
+}
---
+++ quiz_game_page.js
@@ -0,0 +1,5 @@
+function send() {
+localStorage.getItem(Player_1).innerHTML=setitem(player1_name)
+localStorage.getItem(Player_2).innerHTML=setitem(player2_name);
+}
+
